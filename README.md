27077bbe3c6af20baac89845e2feecd50513866d
atbf1dsehw <123597863+atbf1dsehw@users.noreply.github.com>
2024-05-22 14:38:29 +0200

    updates

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# setpinns
-NeurIPS submission
+# SetPINNs
+
+SetPINNs offer a new approach to PINNs, demonstrating that processing neighboring points in the physical domain together can lead to accurate and robust results.
+
+In this repository, we provide code and a demo on how to execute SetPINNs.
+
+Refer to `src/` for the code.
+
+Refer to `setpinns.ipynb` for a demo.
+
+This pipeline is fully inspired by the PINNsFormer implementation, and we have utilized the assets provided by them on: [https://github.com/AdityaLab/pinnsformer](https://github.com/AdityaLab/pinnsformer). Please refer to their GitHub repository to acquire/use them.
+
+
+We also present a comparison between PINNsFormer and SetPINNs. Please refer to `pinnsformer.ipynb` and `setpinns.ipynb` for the comparison. 
+
+On the exact same problem, with the same seed, model, and setup, PINNsFormer ends up with a trivial solution (with a high error rate) and significantly longer runtime (due to convergence issues), whereas SetPINNs provides a very low error rate.
---
+++ src/model/setpinns.py
@@ -0,0 +1,132 @@
+import torch
+import torch.nn as nn
+import pdb
+
+from src.util import get_clones
+
+
+class WaveAct(nn.Module):
+    def __init__(self):
+        super(WaveAct, self).__init__()
+        self.w1 = nn.Parameter(torch.ones(1), requires_grad=True)
+        self.w2 = nn.Parameter(torch.ones(1), requires_grad=True)
+
+    def forward(self, x):
+        return self.w1 * torch.sin(x) + self.w2 * torch.cos(x)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff=256):
+        super(FeedForward, self).__init__()
+        self.linear = nn.Sequential(
+            *[
+                nn.Linear(d_model, d_ff),
+                WaveAct(),
+                nn.Linear(d_ff, d_ff),
+                WaveAct(),
+                nn.Linear(d_ff, d_model),
+            ]
+        )
+
+    def forward(self, x):
+        return self.linear(x)
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, heads):
+        super(EncoderLayer, self).__init__()
+
+        self.attn = nn.MultiheadAttention(
+            embed_dim=d_model, num_heads=heads, batch_first=True
+        )
+        self.ff = FeedForward(d_model)
+        self.act1 = WaveAct()
+        self.act2 = WaveAct()
+        self.enc_attn = None
+
+    def forward(self, x):
+        x2 = self.act1(x)
+        attn = self.attn(x2, x2, x2)
+        x = x + attn[0]
+        self.enc_attn = attn[1]
+        x2 = self.act2(x)
+        x = x + self.ff(x2)
+        return x
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, heads):
+        super(DecoderLayer, self).__init__()
+
+        self.attn = nn.MultiheadAttention(
+            embed_dim=d_model, num_heads=heads, batch_first=True
+        )
+        self.ff = FeedForward(d_model)
+        self.act1 = WaveAct()
+        self.act2 = WaveAct()
+        self.dec_attn = None
+
+    def forward(self, x, e_outputs):
+        x2 = self.act1(x)
+        attn = self.attn(x2, e_outputs, e_outputs)
+        x = x + attn[0]
+        self.dec_attn = attn[1]
+        x2 = self.act2(x)
+        x = x + self.ff(x2)
+        return x
+
+
+class Encoder(nn.Module):
+    def __init__(self, d_model, N, heads):
+        super(Encoder, self).__init__()
+        self.N = N
+        self.layers = get_clones(EncoderLayer(d_model, heads), N)
+        self.act = WaveAct()
+
+    def forward(self, x):
+        for i in range(self.N):
+            x = self.layers[i](x)
+        return self.act(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self, d_model, N, heads):
+        super(Decoder, self).__init__()
+        self.N = N
+        self.layers = get_clones(DecoderLayer(d_model, heads), N)
+        self.act = WaveAct()
+
+    def forward(self, x, e_outputs):
+        for i in range(self.N):
+            x = self.layers[i](x, e_outputs)
+        return self.act(x)
+
+
+class SetPinns(nn.Module):
+    def __init__(self, d_out, d_model, d_hidden, N, heads, in_dim=2):
+        super(SetPinns, self).__init__()
+
+        self.linear_emb = nn.Linear(in_dim, d_model)
+
+        self.encoder = Encoder(d_model, N, heads)
+        self.decoder = Decoder(d_model, N, heads)
+        self.linear_out = nn.Sequential(
+            *[
+                nn.Linear(d_model, d_hidden),
+                WaveAct(),
+                nn.Linear(d_hidden, d_hidden),
+                WaveAct(),
+                nn.Linear(d_hidden, d_out),
+            ]
+        )
+
+    def forward(self, *v):
+        if len(v) > 1:
+            src = torch.cat(v, dim=-1)
+        else:
+            src = v[0]
+        src = self.linear_emb(src)
+        e_outputs = self.encoder(src)
+        d_output = self.decoder(src, e_outputs)
+        output = self.linear_out(d_output)
+        return output
